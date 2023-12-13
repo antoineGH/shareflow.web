09010ed3d9702889fa7145da1f091d5240aaefed
Antoine Ratat <antoine.ratat@gmail.com>
2023-12-13 22:49:34 +1100

    improve arrows breadcrumb

---
+++ src/components/pages/files/breadcrumbs/StyledSeparatorIcon.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material/styles'
+import SvgIcon from '@mui/material/SvgIcon'
+
+const StyledIcon = styled(SvgIcon)({
+  fontSize: '1.1rem',
+  color: 'grey',
+})
+
+export default StyledIcon
