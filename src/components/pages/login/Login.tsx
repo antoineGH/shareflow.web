7b7f9e428f57f6f7fe734c451da56ac5bd443db9
Antoine <35226140+antoineGH@users.noreply.github.com>
2024-01-23 01:59:11 +1100

    Bug fix timestamp#145 (#148)
    * fix responsive loading responsive tab
    
    * fix svg login
    
    * fix svg login
    
    * fix svg login

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -9,11 +9,11 @@ import Paper from '@mui/material/Paper'
 import { alpha } from '@mui/material/styles'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import BackgroundSVG from 'assets/stacked-waves.svg?react'
 
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 
+import BackgroundSVG from 'assets/stacked-waves.svg'
 import { AuthContext } from 'components/auth/AuthContext'
 import { useDispatch } from 'store/hooks'
 import { openSnackbar } from 'store/snackbar/slice'
@@ -87,6 +87,7 @@ const Login = () => {
 
   return (
     <Container
+      id="container-login"
       sx={{
         display: 'flex',
         flexDirection: 'column',
